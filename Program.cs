32db92aefadc059c756f482dc050fe4fe7bca05e
HP <HP@DESKTOP-TV4NOB9>
2022-04-18 23:31:32 -0400

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Diccionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string respuesta = "";
+            do
+            {
+
+                Console.WriteLine("|                                                              |\n");
+                Console.WriteLine("|                    Menu de Opciones                      |\n");
+                Console.WriteLine("|1.- Añadir palabra al diccionario|\n");
+                Console.WriteLine("|2.- Eliminar palabra del diccionario|\n");
+                Console.WriteLine("|3.- Buscar palabra en el diccionario|\n");
+                Console.WriteLine("|4.- ****** Salir *******|\n");
+                //  Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("|      © Derechos reservados diseñado por:         |\n");
+
+                // Console.BackgroundColor = ConsoleColor.Blue;
+                int opcion = int.Parse(Console.ReadLine());
+                switch (opcion)
+                {
+                    case 1:
+                        {
+                            string resp = "";
+                            do
+                            {
+                                Console.WriteLine("Ingrese una palabra");
+                                string dato = Console.ReadLine();
+                                Lista listaUno = new Lista();
+                                listaUno.addList(dato);
+                                listaUno.show();
+                                Console.WriteLine("**************************");
+                                Console.WriteLine("Añadir palabra ... Si o No");
+                                Console.WriteLine("**************************");
+                                resp = Console.ReadLine();
+                            } while (resp != "No");
+                            break;
+                        }
+                    case 2:
+                        {
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("Ingrese una palabra");
+                            break;
+                        }
+                    case 4:
+                        {
+                            break;
+                        }
+                }
+                Console.WriteLine("Realizar otra operación... Si o No ...");
+                respuesta = Console.ReadLine();
+            } while (respuesta != "No");
+            Console.Clear();
+        }
+    }
+}
+
---
+++ lista.cs
@@ -0,0 +1,53 @@
+﻿namespace Diccionario
+{
+    class Lista
+    {
+        // head = cabeza
+        private Nodo head;
+        private Nodo aux;
+
+        // construct 
+        public Lista()
+        {
+            // instanciamos la clase
+            head = new Nodo();
+            // hacemos referencia que la lista inicialmente
+            // estara vacia
+            head.Enlace = null;
+        }
+        public void addList(string datoInsertar)
+        {
+            aux = head;
+            while (aux.Enlace != null)
+            {
+                aux = aux.Enlace;
+            }
+            // Aqui instanciamos la clase nodo y se cree un nuevo nodo
+            Nodo newNodo = new Nodo();
+            newNodo.Dato = datoInsertar;
+            newNodo.Enlace = null;
+            aux.Enlace = newNodo;
+        }
+        // show = mostrar
+        public void show()
+        {
+            aux = head;
+            while (aux.Enlace != null)
+            {
+                aux = aux.Enlace;
+                string dato = aux.Dato;
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.WriteLine(" " + dato + " - ");
+            }
+        }
+        public void edit()
+        {
+
+
+        }
+        public void delete()
+        {
+
+        }
+    }
+}
